f3cc431a5d9e66a28a80e965171d72bf770a9956
Thurein Naing <thuureinnnaingg412@gmail.com>
2025-04-25 07:39:14 +0630

    fix validation error

--- lib/validateBody.ts
+++ lib/validateBody.ts
@@ -1,17 +1,22 @@
-import { ZodError, ZodSchema } from "zod";
+import { ZodError, ZodObject, ZodRawShape, ZodSchema } from "zod";
 
 const validateBody = (
 	body: unknown,
-	schema: ZodSchema,
+	schema: ZodObject<ZodRawShape> | ZodSchema,
 	partial: boolean = false
 ) => {
-	const validatedData = partial
-		? schema.partial().safeParse(body)
-		: schema.safeParse(body);
-	if (!validatedData.success) {
-		throw new ZodError(validatedData.error.issues);
+	if (schema instanceof ZodObject) {
+		const validatedData = partial
+			? schema.partial().safeParse(body)
+			: schema.safeParse(body);
+		if (!validatedData.success) {
+			throw new ZodError(validatedData.error.issues);
+		}
+		return validatedData;
+	} else {
+		// Handle the case where schema is not a ZodObject
+		throw new Error("Schema must be a ZodObject");
 	}
-	return validatedData;
 };
 
 export default validateBody;
