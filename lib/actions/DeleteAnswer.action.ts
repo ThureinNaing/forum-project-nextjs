68629f7ae51fba8eef867e34e0643b33808acca3
Thurein Naing <thuureinnnaingg412@gmail.com>
2026-05-14 12:07:07 +0630

    refactor: add transition  DeleteAnswer server action

--- lib/actions/DeleteAnswer.action.ts
+++ lib/actions/DeleteAnswer.action.ts
@@ -10,31 +10,48 @@ import Question from "@/models/question.model";
 import Vote from "@/models/vote.model";
 import { revalidatePath } from "next/cache";
 import ROUTES from "@/routes";
+import mongoose from "mongoose";
 
 export async function DeleteAnswerAction(params: { answerId: string }) {
 	await dbConnect();
 	const auth_session = await auth();
 	const user = auth_session?.user;
 
+	const session = await mongoose.startSession();
+	session.startTransaction();
+
 	try {
 		const validatedData = validateBody(params, DeleteAnswerSchema);
 		const { answerId } = validatedData.data;
+		if (!auth_session || !auth_session.user) {
+			throw new Error("Unauthorized: Please login first");
+		}
 		const answer = await Answer.findById(answerId);
 		if (!answer) throw new Error("Answer not found");
 
+		if (user?.id !== answer.author.toString()) {
+			throw new Error("Forbidden: You can only delete your answer");
+		}
+
 		await Question.findByIdAndUpdate(
 			answer.question,
-			{
-				$inc: { answers: -1 },
-			},
-			{ new: true },
+			{ $inc: { answers: -1 }, new: true },
+			{ session },
 		);
 
-		await Vote.deleteMany({ type_id: answerId, type: "answer" });
-		await Answer.findByIdAndDelete(answerId);
+		await Vote.deleteMany(
+			{ type_id: answerId, type: "answer" },
+			{ session },
+		);
+		await Answer.findByIdAndDelete(answerId, { session });
+		await session.commitTransaction();
 		revalidatePath(ROUTES.PROFILE(user?.email));
+		revalidatePath(ROUTES.QUESTION_DETAILS(answer.question.toString()));
 		return { success: true };
 	} catch (error) {
+		await session.abortTransaction();
 		return handleActionErrorResponse(error);
+	} finally {
+		await session.endSession();
 	}
 }
